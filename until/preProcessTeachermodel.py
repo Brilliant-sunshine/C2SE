f48009619f0a80fc8b9235683efa591d169bfa65
Brilliant-sunshine <97374544+Brilliant-sunshine@users.noreply.github.com>
2022-01-14 11:45:54 +0800

    Delete until directory

--- until/preProcessTeachermodel.py
+++
@@ -1,4 +0,0 @@
-def preprocess_teacher(model, teacher):
-    for param_m, param_t in zip(model.parameters(), teacher.parameters()):
-        param_t.data.copy_(param_m.data)  # initialize
-        param_t.requires_grad = False  # not update by gradient
